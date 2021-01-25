c4e21a2ce0eb282bdb2b1c56dafb9eb9d6df4213
hkcheung1217 <hoicheung@rocketmail.com>
2021-01-25 01:53:04 -0800

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, 'frontend/build')));
