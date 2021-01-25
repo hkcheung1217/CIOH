8a2cdba1031781746bdc7861f59685d43fcef241
hkcheung1217 <hoicheung@rocketmail.com>
2021-01-25 02:09:19 -0800

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -21,16 +21,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+const dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
+	app.use(express.static(path.join(dirname, '/frontend/build')));
 
-	app.use('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-	});
+	app.get('*', (req, res) => res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html')));
 } else {
-	app.use('/', (req, res) => {
+	app.get('/', (req, res) => {
 		res.send('API is running...');
 	});
 }
